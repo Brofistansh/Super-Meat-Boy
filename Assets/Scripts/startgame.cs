11680871b4c76130afbf2588ad19108d66d7cac0
Brofistansh <jaiansh2016@gmail.com>
2023-06-15 15:26:43 +0530

    scene linked

---
+++ Assets/Scripts/startgame.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startgame : MonoBehaviour
+{
+    public void gam()
+    {
+        SceneManager.LoadScene("gam");
+    }
+    public void star()
+    {
+        SceneManager.LoadScene("star");
+
+    }
+}
